df88ea3cf666cd56c5effcec847d489cdf3d0be4
mohamedAbdelazim <mohamedabdelazim654@gmail.com>
2024-06-01 15:40:18 +0300

    remove-last-update

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -5,14 +5,26 @@ import { userModel } from "../../../databases/models/user.model.js";
 import { ApiFeatures } from "../../utils/apiFeatures.js";
 
 const addUser = catchError(async (req, res, next) => {
-  if (req.file) req.body.profileImg =req.file.filename;
-  let profile = req.body.profileImg?req.body.profileImg:"defaultAvatar.png";
+  
+  let filePath;
+  if(req.file){
+    filePath=req.file.path;
+  }else{
+    filePath="https://kemet-gp2024.onrender.com/defaultAvatar.png";
+  }
 
-  let user = new userModel(req.body);
-  user.profileImg = `https://kemet-gp2024.onrender.com/${profile}`;
-  user.confirmEmail = true;
-  await user.save();
-  res.json({ msg: "success", user });
+  cloudinary.uploader.upload(filePath, async(error,result)=>{
+    let user = new userModel();
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    user.DOB = req.body.DOB;
+    user.city = req.body.city;
+    user.profileImg = result.secure_url;
+    user.password = req.body.password;
+    await user.save();
+    res.json({ msg: "success" });
+  })
 });
 
 const getUsers = catchError(async (req, res, next) => {
@@ -36,12 +48,38 @@ const getUser = catchError(async (req, res, next) => {
 });
 
 const updateUser = catchError(async (req, res, next) => {
-  if (req.file) req.body.profileImg = `https://kemet-gp2024.onrender.com/${req.file.filename}`;
-  let user = await userModel.findByIdAndUpdate(req.params.id, req.body, {
+  if(req.file){
+    cloudinary.uploader.upload(req.file.path,async(error,result)=>{
+    let user = await userModel.findOneAndUpdate({ _id: req.user._id },{
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    DOB: req.body.DOB,
+    city: req.body.city,
+    profileImg: result.secure_url,
+    password: req.body.password,
+  }, {
     new: true,
   });
+
   !user && next(new apiError("not user found", 404));
   user && res.json({ msg: "success", user });
+    })
+  }else{
+  let user = await userModel.findOneAndUpdate({ _id: req.user._id },{
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    DOB: req.body.DOB,
+    city: req.body.city,
+    password: req.body.password,
+  }, {
+    new: true,
+  });
+
+  !user && next(new apiError("not user found", 404));
+  user && res.json({ msg: "success", user });
+  }
 });
 
 const deleteUser = deleteOne(userModel);
