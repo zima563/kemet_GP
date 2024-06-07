12762ee6709e2b66080fcfb4f0ba9023f33a2c00
mohamedAbdelazim <mohamedabdelazim654@gmail.com>
2024-06-07 18:46:57 +0300

    last update

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -35,7 +35,7 @@ const createCashOrder = catchError(async(req,res,next)=>{
     })
 
     await tripModel.bulkWrite(options);
-    await sendEmailOrderConfirm(req.user.email,req.user.firstName+req.user.lastName,order.orderCode)
+    await sendEmailOrderConfirm(req.user.email,req.user.firstName+" "+req.user.lastName,order.orderCode)
     await myTicketModel.findByIdAndDelete(req.params.id);
 
     res.status(200).json({msg: "success", order})
